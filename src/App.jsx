7c8d0fdf97b6a98d6d4d28237f783ae4bfb4748c
Tote1514 <joseantoniocastrom5@gmail.com>
2025-07-29 17:07:05 -0500

    Set a timer to reset the quiz

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ function App() {
   const [userName, setUserName] = useState("");
   const [element, setElement] = useState("");
   const [artwork, setArtWork] = useState(null);
+
+  function resetQuizState() {
+    setCurrentQuestionIndex(0);
+    setAnswers([]);
+    setUserName("");
+    setElement("");
+    setArtWork(null);
+  }
   
 
     const questions = [
@@ -80,54 +88,58 @@ function App() {
       return temp;
   };
 
-  const fetchArtwork = async (element)=>{
-
-    if (element === "") {
-      setArtWork(null);
-      return;
-    }
-
-    console.log("Element:", element);
-
-    const apiUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=";
-
-    const url = `${apiUrl}${element}`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-
-        if (data.total > 0) {
-          const randomIndex = Math.floor(Math.random() * data.total);
-          const objectId = data.objectIDs[randomIndex];
-          return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`);
-        } 
-        else {
-          throw new Error("No artwork found");
-        }
-      })
-      .then(response => response.json())
-      .then(artworkData => {
-        setArtWork(artworkData);
-      })
-      .catch(error => {
-        console.error("Error fetching artwork:", error);
+  const fetchArtwork = async (element) => {
+      if (element === "") {
         setArtWork(null);
-      });
+        return;
+      }
 
-  };
+      console.log("Element:", element);
 
-  useEffect(
-     () => {
-      if (currentQuestionIndex === questions.length) {
-          const selectedElement = determineElement(answers);
-          setElement(selectedElement);
-          fetchArtwork(keywords[selectedElement]);
-        }
-    },
-    [currentQuestionIndex]
-  );
+      const apiUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=";
 
+      const url = `${apiUrl}${element}`;
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
 
+          if (data.total > 0) {
+            const randomIndex = Math.floor(Math.random() * data.total);
+            const objectId = data.objectIDs[randomIndex];
+            return fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`);
+          } 
+          else {
+            throw new Error("No artwork found");
+          }
+        })
+        .then(response => response.json())
+        .then(artworkData => {
+          setArtWork(artworkData);
+        })
+        .catch(error => {
+          console.error("Error fetching artwork:", error);
+          setArtWork(null);
+        });
+
+    };
+
+    useEffect(() => {
+      if (currentQuestionIndex !== questions.length) 
+        return;
+
+      const selectedElement = determineElement(answers);
+      setElement(selectedElement);
+      fetchArtwork(keywords[selectedElement]);
+
+      const timeOutId = setTimeout(() => {
+        resetQuizState();
+        navigateToHome();
+
+      }, 10000);
+
+      return () => clearTimeout(timeOutId);
+  }, [currentQuestionIndex]
+);  
 
   return (
     <UserProvider value ={{name: userName, setName: setUserName}}>
@@ -144,6 +156,14 @@ function App() {
       </Routes>
     </UserProvider>
   )
+
+  function navigateToHome() {
+    window.history.pushState({}, '', '/');
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
+  }
 }
 
 export default App
+
+
